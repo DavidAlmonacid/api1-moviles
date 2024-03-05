bda584c674c3e8d8716e513177939e7ac2ad3150
David Almonacid <david.almonacidf@gmail.com>
2024-03-05 11:11:08 -0500

    Include PUT and DELETE methods

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import fs from "node:fs/promises";
+import { writeFile } from "node:fs/promises";
 import path from "node:path";
 import tasks from "../mocks/tasks.json" assert { type: "json" };
 
@@ -31,12 +31,57 @@ tasksRouter.post("/", async (req, res) => {
   tasks.push(newTask);
 
   try {
-    await fs.writeFile(
+    await writeFile(
       path.resolve(process.cwd(), "src/mocks/tasks.json"),
       JSON.stringify(tasks, null, 2)
     );
 
     res.status(201).json(newTask);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+tasksRouter.put("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedTask = req.body;
+  const index = tasks.findIndex((task) => task.id === id);
+
+  if (index !== -1) {
+    tasks[index] = { ...tasks[index], ...updatedTask };
+
+    try {
+      await writeFile(
+        path.resolve(process.cwd(), "src/mocks/tasks.json"),
+        JSON.stringify(tasks, null, 2)
+      );
+
+      res.json(tasks[index]);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  } else {
+    res.status(404).json({ message: "Task not found" });
+  }
+});
+
+tasksRouter.delete("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = tasks.findIndex((task) => task.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "Task not found" });
+  }
+
+  tasks.splice(index, 1);
+
+  try {
+    await writeFile(
+      path.resolve(process.cwd(), "src/mocks/tasks.json"),
+      JSON.stringify(tasks, null, 2)
+    );
+
+    res.sendStatus(204);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
